8d5c77f251ca8425b0e3a972ae1584d583e20734
DatNguyen0912 <90879290+DatNguyen0912@users.noreply.github.com>
2022-04-19 08:17:00 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            string[] can = { "Canh" , "Tan" , "Nham" , "Quy", "Giap", "At", "Binh", "Dinh", "Mau", "Ky" };
+            string[] chi = {"Than", "Dau", "Tuat", "Hoi", "Ti", "Suu","Dan", "Meo", "Thin", "Ty", "Ngo", "Mui"};
+                 Console.WriteLine( "Nhap nam: " );
+            n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine( "{0} - {1} {2}\n" , n, can[n % 10], chi[n % 12] );
+            Console.WriteLine( "{0} - {1} {2}\n", n + 60, can[n % 10], chi[n % 12] );
+            Console.ReadLine();
+        }
+    }
+}
